a281301287448410c73cbdefd4d8f8c672b23ed2
Veronika D <donets.veronika@yahoo.com>
2019-11-04 18:53:14 +0200

    Updates

---
+++ precache-manifest.95c097bbb62a85264a3e81961c963efe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b94705b79b97a0f772b55902e45997",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "89c68aeb10cfba1ddaa7",
+    "url": "/react_add-todo-form/static/css/main.6d871ab2.chunk.css"
+  },
+  {
+    "revision": "55361b4468bd591bf05f",
+    "url": "/react_add-todo-form/static/js/2.55354b2b.chunk.js"
+  },
+  {
+    "revision": "89c68aeb10cfba1ddaa7",
+    "url": "/react_add-todo-form/static/js/main.e3751d3f.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
--- src/components/User.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function User({ user }) {
-  return <td>{user.name}</td>;
-}
-
-export default User;
